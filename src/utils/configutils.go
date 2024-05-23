7ba7ddb5200b5393709e1f1ddc107af8f89aed7f
Sanchezyu <1871808588@qq.com>
2024-05-23 22:53:07 +0800

    commit config file

---
+++ src/utils/configutils.go
@@ -0,0 +1,17 @@
+package utils
+
+// InitConf 读取配置文件
+func InitConf() {
+	//const ConfigFile = "./setting/config.yaml"
+	//c := &config.Config{}
+	//yamlConfig, err := ioutil.ReadFile(ConfigFile)
+	//if err != nil {
+	//	_ = fmt.Errorf("get config.yaml error: %s", err)
+	//}
+	//err = yaml.Unmarshal(yamlConfig, c)
+	//if err != nil {
+	//	log.Fatalf("config init Unmarshal: %v", err)
+	//}
+	//fmt.Println(c)
+	//global.Config = c
+}
