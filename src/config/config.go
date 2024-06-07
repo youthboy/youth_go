2e77f4366aaf907223e276c13af5d114c9a1f8fe
Sanchezyu <1871808588@qq.com>
2024-06-07 10:17:19 +0800

    config settings

--- src/config/config.go
+++ src/config/config.go
@@ -1,21 +1,17 @@
-package model
+package config
 
 import (
 	"fmt"
 	"github.com/spf13/viper"
-	"os"
 )
 
 var Config struct {
 }
 
 func InitConfig() {
-	viper.SetConfigFile("config.yaml")
+	viper.SetConfigFile("./setting/config.yaml")
 	viper.SetConfigType("yaml")
-	currentPath, _ := os.Getwd()
 	viper.AddConfigPath(".")
-
-	fmt.Println(currentPath)
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Sprintf("Load Config Error : %s", err.Error()))
--- src/main.go
+++ src/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
+	"youthmod/config"
 )
 
 func main() {
+	config.InitConfig()
+	fmt.Println(viper.GetString("mysql.host"))
 	fmt.Print("这是我第一个GO程序")
 
 }
